259278d4ec8efd0fcdc142699b8ce687c6cda8cb
tekyhost44 <borisr@tekyhost.com>
2024-08-04 11:33:05 -0400

    Update restart_service.py

--- restart_service.py
+++ restart_service.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+from datetime import datetime
 import subprocess
 import sys
 
@@ -11,8 +11,21 @@ def service_status():
         status_httpd = subprocess.call(["systemctl", "is-active", "--quiet", "httpd"])
         if status_mariadb != 0:
                 subprocess.call(["systemctl", "start", "mariadb"])
+                srvc = "mariadb"
+                add_to_log(srvc)
         if status_httpd != 0:
                 subprocess.call(["systemctl", "start", "httpd"])
+                srvc = "httpd"
+                add_to_log(srvc)
+def add_to_log(srvc):
+        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        f = open("/var/log/tekyhost.log", "a")
+        f.write(" " + '\n')
+        f.write(current_time + '\n')
+        f.write(srvc + " service restarted" + '\n')
+        f.write("-------------------------" + '\n')
+        f.write(" " + '\n')
+        f.close()
 
 if __name__=="__main__":
         main()
